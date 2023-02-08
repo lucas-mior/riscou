b080efe0a6dfed81e18b2fa74f09f0c1de05c8b1
lucas-mior <lucas.mior@tutamail.com>
2023-02-08 19:46:49 -0300

    put everything in main()

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,46 @@ fn main() {
         Err(_) => {usage(); exit(1)},
     };
 
+    let filename = path;
     let extras = Vec::from_iter(argv[2..].iter().cloned());
-    preview(&path, &extras);
+    for rule in RULES {
+        let mime = rule.mime();
+        let comp_conf;
+        let comp_file: String;
+        // let mut comp_file;
+        if mime.len() >= 4 && &mime[0..5] == "fpath" { 
+            comp_conf = &mime[5..];
+            comp_file = filename.to_string();
+        } else {
+            comp_conf = &mime;
+            let path = Path::new(&filename);
+            comp_file = tree_magic::from_filepath(path);
+        }
+        let r_conf = Regex::new(comp_conf).unwrap();
+        if r_conf.is_match(&comp_file) {
+            let r_extras = Regex::new(r"%riscou-extra([0-9])%").unwrap();
+            let args: &[&str] = rule.args().clone();
+            let mut cargs: Vec<&str> = vec![];
+            for arg in args.iter() {
+                if arg == &("%riscou-filename%") {
+                    cargs.push(&filename);
+                } else if r_extras.is_match(arg) {
+                    let caps = r_extras.captures(arg).unwrap();
+                    let x = caps.get(1).map_or("", |m| m.as_str());
+                    let i = x.parse::<usize>().unwrap();
+                    if i < extras.len() {
+                        cargs.push(&extras[i]);
+                    }
+                } else {
+                    cargs.push(arg);
+                }
+            }
+            Command::new(cargs[0]).args(cargs[1..].iter()).exec();
+            break;
+        } else {
+            continue;
+        }
+    }
 }
 
 named_tuple!(
@@ -71,44 +109,3 @@ Rule(("audio/.*",               &["vid.sh", "%riscou-filename%", "%riscou-extra0
 Rule(("video/.*",               &["vid.sh", "%riscou-filename%", "%riscou-extra0%", "%riscou-extra1%", "%riscou-extra2%", "%riscou-extra3%"])),
 Rule((".+",                     &["file", "--mime-type", "%riscou-filename%"])),
 ];
-
-fn preview(filename: & String, extras: & Vec<String>) {
-    for rule in RULES {
-        let mime = rule.mime();
-        let comp_conf;
-        let comp_file: String;
-        // let mut comp_file;
-        if mime.len() >= 4 && &mime[0..5] == "fpath" { 
-            comp_conf = &mime[5..];
-            comp_file = filename.to_string();
-        } else {
-            comp_conf = &mime;
-            let path = Path::new(filename);
-            comp_file = tree_magic::from_filepath(path);
-        }
-        let r_conf = Regex::new(comp_conf).unwrap();
-        if r_conf.is_match(&comp_file) {
-            let r_extras = Regex::new(r"%riscou-extra([0-9])%").unwrap();
-            let args: &[&str] = rule.args().clone();
-            let mut cargs: Vec<&str> = vec![];
-            for arg in args.iter() {
-                if arg == &("%riscou-filename%") {
-                    cargs.push(filename);
-                } else if r_extras.is_match(arg) {
-                    let caps = r_extras.captures(arg).unwrap();
-                    let x = caps.get(1).map_or("", |m| m.as_str());
-                    let i = x.parse::<usize>().unwrap();
-                    if i < extras.len() {
-                        cargs.push(&extras[i]);
-                    }
-                } else {
-                    cargs.push(arg);
-                }
-            }
-            Command::new(cargs[0]).args(cargs[1..].iter()).exec();
-            break;
-        } else {
-            continue;
-        }
-    }
-}

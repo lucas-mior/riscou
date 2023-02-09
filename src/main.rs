628c240979ebbfbefabdf5cbcff0d767c872dc0b
lucas-mior <lucas.mior@tutamail.com>
2023-02-09 14:52:11 -0300

    dont use named tuple, use only tuple

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::fs;
 use std::env;
 use std::path::Path;
 use regex::Regex;
-use named_tuple::named_tuple;
 use std::process::Command;
 use std::os::unix::process::CommandExt;
 
@@ -33,10 +32,9 @@ fn main() {
     let filename = path;
     let extras = Vec::from_iter(argv[2..].iter().cloned());
     for rule in RULES {
-        let mime = rule.mime();
+        let mime = rule.0;
         let comp_conf;
         let comp_file: String;
-        // let mut comp_file;
         if mime.len() >= 4 && &mime[0..5] == "fpath" { 
             comp_conf = &mime[5..];
             comp_file = filename.to_string();
@@ -50,7 +48,7 @@ fn main() {
         if r_conf.is_match(&comp_file) {
             let r_extras = Regex::new(r"%riscou-extra([0-9])%").unwrap();
             let r_filename = Regex::new(r".*(%riscou-filename%).*").unwrap();
-            let args: &[&str] = rule.args().clone();
+            let args: &[&str] = rule.1.clone();
             let mut cargs: Vec<&str> = vec![];
             for arg in args.iter() {
                 if r_filename.is_match(arg) {
@@ -82,42 +80,6 @@ fn main() {
     }
 }
 
-named_tuple!(
-    #[derive(Clone, Debug)]
-    struct Rule<'a> {
-        mime: &'a str,
-        args: &'static [&'static str],
-
-    }
-);
-
-static RULES: &[Rule<'_>] = &[
-Rule(("fpath v\\S[1,3]::",     &["vfile.sh", "%riscou-filename%"])),
-Rule(("fpath g\\S[1,3]::",     &["gdir.sh", "%riscou-filename%"])),
-Rule(("fpath .+\\.fen$",       &["fen.sh", "%riscou-filename%"])),
-Rule(("text/.*",               &["bat", "-p", "--pager=never", "--color=always", "%riscou-filename%"])),
-Rule(("inode/directory",       &["ls", "-1A", "--color", "%riscou-filename%"])),
-Rule(("ms(word|-exce|-powe)",  &["printf", "💩💩💩💩💩💩💩💩💩\n%s\n💩💩💩💩💩💩💩💩💩", "%riscou-filename%"])),
-Rule(("opendoc.+spreadsheet",  &["ods.sh", "%riscou-filename%"])),
-Rule(("officed.+spreadsheet",  &["xlsx.sh", "%riscou-filename%"])),
-Rule(("office.+word",          &["docx.sh", "%riscou-filename%", "%riscou-extra0%"])),
-Rule(("office.+pres",          &["ppt.sh", "%riscou-filename%", "%riscou-extra0%"])),
-Rule(("opendocument",          &["odt2txt", "%riscou-filename%"])),
-Rule(("application/pdf",       &["pdf.sh", "%riscou-filename%", "%riscou-extra0%", "%riscou-extra1%", "%riscou-extra2%", "%riscou-extra3%"])),
-Rule(("application/csv",       &["column", "-t", "-s", ",", "%riscou-filename%"])),
-Rule(("application/json",      &["head", "-n", "40", "%riscou-filename%"])),
-Rule(("application/.*execu.+", &["objdump", "-T", "%riscou-filename%", "%riscou-extra0%"])),
-Rule(("application/x-objec.+", &["objdump", "-T", "%riscou-filename%", "%riscou-extra0%"])),
-Rule(("application/zip",       &["unzip", "-l", "%riscou-filename%"])),
-Rule(("application/gzip",      &["tar", "tf", "%riscou-filename%"])),
-Rule(("application/x-subrip",  &["/usr/bin/cat", "%riscou-filename%"])),
-Rule(("fpath .+\\.ff$",        &["stiv", "%riscou-filename%", "%riscou-extra0%", "%riscou-extra1%", "%riscou-extra2%", "%riscou-extra3%"])),
-Rule(("fpath .+\\.[1-9]$",     &["man", "%riscou-filename%"])),
-Rule(("image/.*dwg",           &["stat", "%riscou-filename%"])),
-Rule(("image/.*xml",           &["head", "-n", "40", "%riscou-filename%"])),
-Rule(("image/.*",              &["stiv", "%riscou-filename%", "%riscou-extra0%", "%riscou-extra1%", "%riscou-extra2%", "%riscou-extra3%"])),
-Rule(("application/x-riff",    &["stiv", "%riscou-filename%", "%riscou-extra0%", "%riscou-extra1%", "%riscou-extra2%", "%riscou-extra3%"])),
-Rule(("audio/.*",              &["vid.sh", "%riscou-filename%", "%riscou-extra0%", "%riscou-extra1%", "%riscou-extra2%", "%riscou-extra3%"])),
-Rule(("video/.*",              &["vid.sh", "%riscou-filename%", "%riscou-extra0%", "%riscou-extra1%", "%riscou-extra2%", "%riscou-extra3%"])),
-Rule((".+",                    &["file", "--mime-type", "%riscou-filename%"])),
+static RULES: &[(&str, &'static [&'static str])] = &[
+(".+",                    &["file", "--mime-type", "%riscou-filename%"]),
 ];
